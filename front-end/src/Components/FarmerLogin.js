bd529a9133382760205bec9b2cd73fe8bd0d3234
Psage23 <pamelasage3@gmail.com>
2020-01-07 14:07:29 -0700

    dependencies added

--- front-end/src/Components/FarmerLogin.js
+++ front-end/src/Components/FarmerLogin.js
@@ -1,34 +1,52 @@
-import React from 'react'
-import { axiosWithAuth } from '../Utils/axiosWithAuth';
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-class Login extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            //username and password
-        }
-    }
+const SigninForm = ({ errors, touched }) => {
+  return (
+    <Form>
+      <div>
+        {touched.email && error.email && <p>{error.emial}</p>}
+        <Field type="email" name="email" placeholder="Email Address" />
+      </div>
+      <div>
+        {touched.password && error.password && <p>{error.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+      <button>Sign In</button>
+    </Form>
+  );
+};
 
-handleChange = e => {
-    //add this.setState({credentials: {...this.state.credentials, [e.target.name]: e.target.value})
-    }
+const FormikSigninForm = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
 
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(7, "Password must be 6 characters or longer")
+      .required("Password is required")
+  }),
 
-handleSubmit = e => {
-    e.prevent.default();
-    axiosWithAuth()
-        .post()
-        .then(res => {
-            localStorage.setItem("token", res.data.payload);
-        })
-        .catch(err => console.log(err));
-}
-render() {
-    return (
-        <div>
-            {/* add login form here */}
-        </div>
-    );
-}
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log("submiting", values);
+    axios
+      .post("", values)
+      .then(res => {
+        console.log("success", res);
+        setStatus(res.data);
+        resetForm();
+      })
+      .catch(err => console.log(err.res));
+  }
+})(SigninForm);
 
-}
+export default FormikSigninForm;
